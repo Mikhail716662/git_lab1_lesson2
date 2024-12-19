bb627777d3b447118f7e04804f344ebed0f6effe
Mikhail <numizmataleks@gmail.com>
2024-12-19 18:51:08 +0300

    создал робота

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import pygame
+
+
+def main():
+    # pygame setup
+    pygame.init()
+    size = width, height = (1280, 720)
+    screen = pygame.display.set_mode(size)
+    screen.fill("purple")
+    clock = pygame.time.Clock()
+    image = load_image('owls.png', -1)
+    robot = load_image('robot.png', -1)
+    image1 = pygame.transform.scale(robot, (500, 100))
+    screen.blit(image1, (100, 200))
+    image1 = pygame.transform.scale(robot, (100, 500))
+    screen.blit(image1, (400, 200))
+    image1 = pygame.transform.scale(robot, (700, 100))
+    screen.blit(image1, (700, 200))
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                screen.blit(image, event.pos)
+        # RENDER YOUR GAME HERE
+
+        pygame.display.flip()
+        clock.tick(60)  # limits FPS to 60
+    pygame.quit()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('images', name)
+    if not os.path.isfile(fullname):
+        print(f'Файл с изображением {fullname} не найден')
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()  # создаёт новую копию изображения с таким же форматом пикселей, как у экрана
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()  # метод для преобразования изображения с сохранением информации о прозрачности
+    return image
+
+
+if __name__ == '__main__':
+    main()
